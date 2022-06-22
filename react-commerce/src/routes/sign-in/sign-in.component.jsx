74a18b443dc01f29f9debb96eddd209b31c9fc12
Michael Tanczos <mike@logic-gate.com>
2022-06-22 15:55:02 -0400

    Lesson 7

--- react-commerce/src/routes/sign-in/sign-in.component.jsx
+++ react-commerce/src/routes/sign-in/sign-in.component.jsx
@@ -1,8 +1,27 @@
+import { useEffect } from 'react';
+import { getRedirectResult } from 'firebase/auth';
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
+
 import { 
+    auth,
     signInWithGooglePopup,
+    signInWithGoogleRedirect,
     createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils'
 
 const SignIn = () => {
+
+    useEffect(() => {
+        async function getResult()
+        {
+            const response = await getRedirectResult(auth);
+            console.log(response);
+            if (response)
+            {
+                const userDocRef = await createUserDocumentFromAuth(response.user);
+            }
+        }
+        getResult();
+    }, []);
 
     const logGoogleUser = async() => {
         const { user }= await signInWithGooglePopup();
@@ -14,6 +33,8 @@ const SignIn = () => {
         <div>
             <h1>Sign In Page</h1>
             <button onClick={logGoogleUser}>Sign In With Google Popup</button>
+            <button onClick={signInWithGoogleRedirect}>Sign In With Google Redirect</button>
+            <SignUpForm/>
         </div>
     )
 }
--- react-commerce/src/utils/firebase/firebase.utils.js
+++ react-commerce/src/utils/firebase/firebase.utils.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app';
-import {getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider} from 'firebase/auth';
+import {getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword} from 'firebase/auth';
 import {
     getFirestore,
     doc,
@@ -27,9 +27,13 @@ const firebaseConfig = {
 
   export const auth = getAuth();
   export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+  export const signInWithGoogleRedirect = () => signInWithRedirect(auth, provider);
 
   export const db = getFirestore();
-  export const createUserDocumentFromAuth = async (userAuth) => {
+  export const createUserDocumentFromAuth = async (userAuth, additionalInformation = {}) => {
+
+    if (!userAuth) return;
+
     const userDocRef = doc(db, 'users', userAuth.uid)
 
     console.log(userDocRef);
@@ -46,7 +50,8 @@ const firebaseConfig = {
             await setDoc(userDocRef, {
                 displayName,
                 email,
-                createdAt
+                createdAt,
+                ...additionalInformation
             });
         } catch (error) {
             console.log('error creating the user', error.message);
@@ -54,4 +59,10 @@ const firebaseConfig = {
     }
 
     return userDocRef;
+  }
+
+  export const createAuthUserWithEmailAndPassword = async (email, password) => {
+    if (!email || !password) return;
+
+    return await createUserWithEmailAndPassword(auth, email, password);
   }
